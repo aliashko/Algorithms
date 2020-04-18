2689ff55c4a2c3a727376be5fc9ac15ca98cb8d7
alex <xppl112@gmail.com>
2020-04-18 13:05:01 +0300

    Search in string (naive algorithm)

---
+++ src/Algorithms/Strings/SearchInString.cs
@@ -0,0 +1,40 @@
+﻿using DataStructures.Arrays;
+
+namespace Algorithms.Strings
+{
+    public static class SearchInString
+    {
+        public static ArrayList<int> NativeSearch(string text, string pattern)
+        {
+            var matchesIndexes = new ArrayList<int>();
+
+            for(int i=0; i<=text.Length - pattern.Length; i++)
+            {
+                var matchFound = true;
+                for(int j = 0; j < pattern.Length; j++)
+                {
+                    if (text[i + j] != pattern[j])
+                    {
+                        matchFound = false;
+                        break;
+                    }
+                }
+                if (matchFound)
+                {
+                    matchesIndexes.Add(i);
+                }
+            }
+
+            return matchesIndexes;
+        }
+
+        public static ArrayList<int> RabinKarpSearch(string text, string pattern)
+        {
+            var matchesIndexes = new ArrayList<int>();
+
+            //TODO: Implement algoruthm
+
+            return matchesIndexes;
+        }
+    }
+}
---
+++ tests/Algorithms.UnitTests/Strings/SearchInStringTests.cs
@@ -0,0 +1,61 @@
+﻿using Algorithms.Strings;
+using DataStructures.Arrays;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UnitTests.Arrays
+{
+    [TestClass]
+    public class SearchInStringTests
+    {
+        [TestMethod]
+        public void TestNativeSearch() => TestSearch(SearchInString.NativeSearch);
+
+        [TestMethod]
+        public void TestRabinKarpSearch() => TestSearch(SearchInString.RabinKarpSearch);
+
+        private void TestSearch(Func<string, string, ArrayList<int>> searchAlgorythm)
+        {
+            var random = new Random();
+            var stringsArray = new string[] { "A", "ab", "abc", "abcd", "abcdef", "DeF", "Sub string", "The Naive String Matching algorithm slides the pattern one by one", "(_)", "幼稚的字符串匹配算法可將模式一一滑動" };
+            var delimitersArray = new char[] { ' ', ',' };
+
+            for (int i = 0; i < 2000; i++)
+            {
+                var textBuilder = new StringBuilder();
+                for (int j = 0; j < i / 10; j++)
+                {
+                    var word = stringsArray[random.Next(stringsArray.Length)];
+                    var delimiter = delimitersArray[random.Next(delimitersArray.Length)];
+                    textBuilder.Append(word);
+                    if (i % 3 != 0)
+                    {
+                        textBuilder.Append(delimiter);
+                    }
+                }
+
+                var text = textBuilder.ToString();
+                var pattern = stringsArray[random.Next(stringsArray.Length)];
+                if (i % 3 == 0 && pattern.Length > 1)
+                {
+                    pattern = pattern.Substring(0, random.Next(pattern.Length - 1));
+                }
+
+                var matches = searchAlgorythm(text, pattern).ToArray();
+                var expectedMatches = ControlSearchInStringAlgoryth(text, pattern);
+
+                Assert.AreEqual(expectedMatches.Length, matches.Length);
+                CollectionAssert.AreEqual(expectedMatches, matches);
+            }
+        }
+
+        private int[] ControlSearchInStringAlgoryth(string text, string pattern)
+        {
+            pattern = Regex.Escape(pattern);
+            return Regex.Matches(text, pattern).Cast<Match>().Select(m => m.Index).ToArray();
+        }
+    }
+}

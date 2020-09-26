3b087703e79b3353f9752b78b2ae0992a70195d8
Alexey <alexey.liashko@gmail.com>
2020-09-26 15:19:10 +0300

    Tries

---
+++ src/DataStructures/Trees/Trie.cs
@@ -0,0 +1,120 @@
+﻿using System;
+
+namespace DataStructures.Trees
+{
+    /// <summary>
+    /// Fast retrieving, inserting - O(key.length)
+    /// But require a lot of memory
+    /// </summary>
+    public class Trie
+    {
+        // Alphabet size (# of symbols) 
+        static readonly int ALPHABET_SIZE = 26;
+
+        public Trie()
+        {
+            RootNode = new TrieNode(default);
+        }
+
+        public TrieNode RootNode { get; private set; }
+
+        public TrieNode FindNodeByKey(string key)
+        {
+            var currentNode = RootNode;
+
+            foreach(var keyChar in key)
+            {
+                var nextNode = currentNode.Children[CharToIndex(keyChar)];
+                if (nextNode == null) return null;
+                currentNode = nextNode;
+            }
+
+            if (!currentNode.IsEndOfWord) return null;
+            return currentNode;
+        }
+
+        public TrieNode Insert(string key)
+        {
+            var currentNode = RootNode;
+
+            foreach (var keyChar in key)
+            {
+                var charIndex = CharToIndex(keyChar);
+                var nextNode = currentNode.Children[charIndex];
+                if(nextNode == null)
+                {
+                    nextNode = currentNode.Children[charIndex] = new TrieNode(keyChar);
+                }
+                currentNode = nextNode;
+            }
+            if (currentNode.IsEndOfWord) throw new ArgumentException("Key is already exist");
+            currentNode.SetAsEndOfWord();
+            return currentNode;
+        }
+
+        public void Remove(string key)
+        {
+            RemoveRecursively(RootNode, key);
+        }
+
+        private void RemoveRecursively(TrieNode subTreeRootNode, string key, int depth = 0)
+        {
+            var charIndex = CharToIndex(key[depth]);
+            var subTreeNode = subTreeRootNode.Children[charIndex];
+            if (subTreeNode == null) throw new ArgumentException("Key doesn't exists");
+
+            if (depth + 1 == key.Length)
+            {
+                if (!subTreeNode.IsEndOfWord) throw new ArgumentException("Key doesn't exists");
+                subTreeNode.SetAsWalkThroughNode();
+            }
+            else {
+                RemoveRecursively(subTreeNode, key, depth + 1);
+            }
+
+            //Remove empty node
+            if (IsEmptyNode(subTreeNode))
+            {
+                subTreeRootNode.Children[charIndex] = null;
+            }
+        }
+
+        private bool IsEmptyNode(TrieNode node)
+        {
+            if (node == null) return true;
+
+            for(int i = 0; i < ALPHABET_SIZE; i++)
+            {
+                if (node.Children[i] != null) return false;
+            }
+
+            return true;
+        }
+
+        private int CharToIndex(char keyChar)
+        {
+            var charIndex = keyChar - 'a';
+            if (charIndex < 0 || charIndex > ALPHABET_SIZE) throw new ArgumentException("Symbol in key is out of range of acceptable symbols");
+            return charIndex;
+        }
+
+        public class TrieNode
+        {
+            public TrieNode(char key)
+            {
+                Key = key;
+                Children = new TrieNode[ALPHABET_SIZE];
+            }
+
+            public char Key { get; }
+
+            public bool IsEndOfWord { get; private set; }
+
+            public TrieNode[] Children { get; }
+
+            public void SetAsEndOfWord() => IsEndOfWord = true;
+
+            public void SetAsWalkThroughNode() => IsEndOfWord = false;
+        }
+    }
+}
---
+++ tests/DataStructures.UnitTests/Trees/TrieTests.cs
@@ -0,0 +1,91 @@
+﻿using DataStructures.Trees;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DataStructures.UnitTests.Trees
+{
+    [TestClass]
+    public class TrieTests
+    {
+        private Trie sampleTrie;
+        private List<string> sampleKeys = new List<string> {
+            "a", "abc", "z", "zoo",
+            "trie", "is", "an", "efficient", "information", "retrieval", "data", "structure"
+        };
+
+        [TestInitialize]
+        public void Setup()
+        {
+            sampleTrie = new Trie();
+            sampleKeys.ForEach(key => sampleTrie.Insert(key));
+        }
+
+        [TestMethod]
+        public void AddedKeysCanBeFoundInTrie()
+        {
+            foreach(var key in sampleKeys)
+            {
+                var node = sampleTrie.FindNodeByKey(key);
+                Assert.IsNotNull(node);
+                Assert.AreEqual(key.Last(), node.Key);
+                Assert.IsTrue(node.IsEndOfWord);
+            }
+        }
+
+        [TestMethod]
+        public void UnknownKeysCannotBeFoundInTrie()
+        {
+            var unknownKeys = new List<string> { "b", "abd", "informatiom", "ant", "antic", "tri", "retrie" };
+            foreach (var key in unknownKeys)
+            {
+                var node = sampleTrie.FindNodeByKey(key);
+                Assert.IsNull(node);
+            }
+        }
+
+        [TestMethod]
+        public void StringKeyCanBeInsertedInTrie()
+        {
+            var newKeys = new List<string> { "zoom", "antic", "b" };
+            foreach (var key in newKeys)
+            {
+                var node = sampleTrie.Insert(key);
+                Assert.IsNotNull(node);
+                Assert.AreEqual(key.Last(), node.Key);
+                Assert.IsTrue(node.IsEndOfWord);
+            }
+        }
+
+        [TestMethod]
+        public void DuplicatedKeyCannotBeInsertedInTrie()
+        {
+            foreach (var key in sampleKeys)
+            {
+                Assert.ThrowsException<ArgumentException>(() => sampleTrie.Insert(key));
+            }            
+        }
+
+        [TestMethod]
+        public void ExistingNodeCanBeRemovedFromTrie()
+        {
+            foreach (var key in sampleKeys)
+            {
+                sampleTrie.Remove(key);
+                var node = sampleTrie.FindNodeByKey(key);
+                Assert.IsNull(node);
+            }
+        }
+
+        [TestMethod]
+        public void NonexistingNodeCannotBeRemovedFromTrie()
+        {
+            var unknownKeys = new List<string> { "b", "abd", "informatiom", "ant", "antic", "tri", "retrie" };
+            foreach (var key in unknownKeys)
+            {
+                Assert.ThrowsException<ArgumentException>(() => sampleTrie.Remove(key));
+            }
+        }
+    }
+}
